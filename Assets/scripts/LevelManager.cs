d031c98654c0a6a5f20b4dd0b6bbbc8590988556
Fonwicket <fonwicket@gmail.com>
2018-06-10 08:42:09 +0200

    overzetten

---
+++ Assets/scripts/LevelManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public float autoLoadNextLevelAfter;
+
+
+
+    void Start()
+    {
+        if (autoLoadNextLevelAfter == 0)
+        {
+            Debug.Log("Level auto load disabled");
+        }
+        else
+        {
+            Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+        }
+    }
+
+
+
+    public void QuitRequest() {
+        Debug.Log("Quit requested");
+        Application.Quit();
+    }
+
+    public void LoadNextLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadLevel(string name)
+
+    {
+        Debug.Log("New level load: " + name);
+        SceneManager.LoadScene(name);
+    }
+
+}
---
+++ Assets/scripts/MusicManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] levelMusicChangeArray;
+
+	private AudioSource audioSource;
+
+	// Use this for initialization;
+	void Awake() {
+		DontDestroyOnLoad(gameObject);
+		Debug.Log("Don't destroy on load: " + name);
+		
+		
+	}
+
+	void Start(){
+		audioSource = GetComponent<AudioSource> ();
+	}
+
+	void OnLevelWasLoaded(int level){
+        AudioClip thisLevelMusic = levelMusicChangeArray[level];
+
+        Debug.Log("Playing clip: " + thisLevelMusic);
+
+        if (thisLevelMusic) { // If there is some music attached
+            audioSource.clip = thisLevelMusic;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeVolume(float volume) {
+        audioSource.volume = volume;
+    }
+}
